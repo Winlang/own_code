8bd142d1806b701e21631b7fdcaf48230949e9c3
Winlang <rowen0820@gmail.com>
2014-06-06 16:20:07 +0800

    another way to fibonacci

--- feibonaqie.py
+++ feibonaqie.py
@@ -1,15 +1,28 @@
-class Fib(object):
-	def __init__(self):
-		self.a,self.b=0,1 
+# class Fib(object):
+# 	def __init__(self):
+# 		self.a,self.b=0,1 
 	
-	def __iter__(self):	
-		return self
+# 	def __iter__(self):	
+# 		return self
 	
-	def next(self):
-		self.a,self.b=self.b,self.a+self.b
-		if self.a>1000:
-			raise StopIteration();
-		return self.a
+# 	def next(self):
+# 		self.a,self.b=self.b,self.a+self.b
+# 		if self.a>1000:
+# 			raise StopIteration();
+# 		return self.a
 
-for n in Fib():
-	print n
+# for n in Fib():
+# 	print n
+
+
+
+def fib(n):
+	result = []
+	a,b = 0,1
+	n = int(raw_input('please enter the top of the list'))
+	while b < n:
+		result.append(b)
+		a,b = b,a+b
+	return result
+
+print fib(100)
