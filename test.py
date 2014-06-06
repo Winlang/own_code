dc4bfab9c24828d0ce9f3ac405b3335adb688868
Winlang <rowen0820@gmail.com>
2014-06-06 15:31:49 +0800

    __str__ example

--- test.py
+++ test.py
@@ -9,19 +9,28 @@
 #print fact(5)
 
 
+#class Student(object):
+#	@property
+#	def score(self):
+#		return self.__score
+#
+#	@score.setter
+#	def score(self,value):
+#		if not isinstance(value,int):
+#			raise ValueError('score must be an integer!')
+#		if value <0 or value >100:
+#			raise ValueError('score must between 0~100')
+#		self.__score = value
+#
+#s = Student()
+#s.score = 60
+#print s.score
+
 class Student(object):
-	@property
-	def score(self):
-		return self.__score
-
-	@score.setter
-	def score(self,value):
-		if not isinstance(value,int):
-			raise ValueError('score must be an integer!')
-		if value <0 or value >100:
-			raise ValueError('score must between 0~100')
-		self.__score = value
-
-s = Student()
-s.score = 60
-print s.score
+	def __init__(self,name):
+		self.name = name
+	
+	def __str__(self):
+		return 'Student object(name:%s)'%self.name
+	
+print Student('rowen')
