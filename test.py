e0fed4f9e57eb83b9ae80b2417eeb24a4eb066ec
Winlang <rowen0820@gmail.com>
2014-06-06 13:38:26 +0800

    add a @property feature

--- test.py
+++ test.py
@@ -1,9 +1,27 @@
-def fact(n):
-	return fact_iter(1,1,n)
+#def fact(n):
+#	return fact_iter(1,1,n)
+#
+#def fact_iter(product,count,max):
+#	if count > max:
+#		return product
+#	return fact_iter(product * count,count + 1,max)	
+#
+#print fact(5)
 
-def fact_iter(product,count,max):
-	if count > max:
-		return product
-	return fact_iter(product * count,count + 1,max)	
 
-print fact(5)
+class Student(object):
+	@property
+	def score(self):
+		return self.__score
+
+	@score.setter
+	def score(self,value):
+		if not isinstance(value,int):
+			raise ValueError('score must be an integer!')
+		if value <0 or value >100:
+			raise ValueError('score must between 0~100')
+		self.__score = value
+
+s = Student()
+s.score = 60
+print s.score
