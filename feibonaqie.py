992958c58c501fe45c6ca09ffd002717677d4b9d
Winlang <rowen0820@gmail.com>
2014-06-06 16:47:22 +0800

    another fibonacci way

--- feibonaqie.py
+++ feibonaqie.py
@@ -16,13 +16,24 @@
 
 
 
-def fib(n):
-	result = []
-	a,b = 0,1
-	n = int(raw_input('please enter the top of the list'))
-	while b < n:
-		result.append(b)
-		a,b = b,a+b
-	return result
+# def fib(n):
+# 	result = []
+# 	a,b = 0,1
+# 	n = int(raw_input('please enter the top of the list'))
+# 	while b < n:
+# 		result.append(b)
+# 		a,b = b,a+b
+# 	return result
 
-print fib(100)
+# print fib(100)
+
+
+class Fib(object):
+	def __getitem__(self,n):
+		a,b=1,1
+		for x in range(n):
+			a,b=b,a+b
+		return a
+
+f=Fib()
+print f[2]
