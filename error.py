fbf12e83c87ff89ac08705e6d425d3d848c6f25a
Winlang <rowen0820@gmail.com>
2014-06-09 23:55:57 +0800

    raise error

--- error.py
+++ error.py
@@ -45,18 +45,31 @@
 
 
 
-import logging
+# import logging
+
+# def foo(s):
+# 	return 10/int(s)
+
+# def bar(s):
+# 	return foo(s)*2
+
+# def main():
+# 	try:
+# 		bar('0')
+# 	except StandardError,e:
+# 		logging.exception(e)
+# main()
+# print 'END'
+
+
+
+class FooError(StandardError):
+	pass
 
 def foo(s):
-	return 10/int(s)
+	n = int(s)
+	if n==0:
+		raise FooError('invalid value : %s,' %s)
+	return 10/n
 
-def bar(s):
-	return foo(s)*2
-
-def main():
-	try:
-		bar('0')
-	except StandardError,e:
-		logging.exception(e)
-main()
-print 'END'
+print foo(0)
