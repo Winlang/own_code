58827b3ed8d3e6a595bdc71e4d18e96983ebdf36
Winlang <rowen0820@gmail.com>
2014-06-09 22:10:43 +0800

    error etc

--- error.py
+++ error.py
@@ -1,9 +1,44 @@
-try:
-	print 'try...'
-	r = 10/1
-	print 'result:',r
-except ZeroDivisionError,e:
-	print 'except:',e
-finally:
-	print 'finally...'
-print 'END'
+# try:
+# 	print 'try...'
+# 	r = 10/1  #r=10/0    
+# 	print 'result:',r
+# except ZeroDivisionError,e:
+# 	print 'except:',e
+# finally:
+# 	#print 'finally...'
+# 	r = 10/0
+# print 'END'
+
+
+
+
+
+# def foo(s):
+# 	return 10/int(s)
+
+# def bar(s):
+# 	return foo(s)*2
+
+# def main():
+# 	try:
+# 		bar('0')
+# 	except StandardError,e:
+# 		print 'Error!'
+# 	finally:
+# 		print 'finally...'
+
+# main()
+
+
+
+
+
+def foo(s):
+	return 10/int(s)
+
+def bar(s):
+	return foo(s)*2
+
+def main():
+	bar('0')
+main()
