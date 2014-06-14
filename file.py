27a3d6b8b2df35c8c1c06c7fa85c2b36049dcc08
Winlang <rowen0820@gmail.com>
2014-06-14 23:11:58 +0800

    write test

--- file.py
+++ file.py
@@ -1,5 +1,8 @@
 #test
-f=open('./danyuanceshi.py','r')
-d = f.read()
-print d
+#f=open('./danyuanceshi.py','r')
+#d = f.read()
+#print d
 
+f = open('./text.txt','w')
+f.write('this is write test');
+f.close()
