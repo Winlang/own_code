787fc85eaeb516208f029c47817d9194ebfd3a4d
Winlang <rowen0820@gmail.com>
2014-06-15 23:21:42 +0800

    fix

--- serialize.py
+++ serialize.py
@@ -19,9 +19,28 @@
 
 #Json
 
-import json
+# import json
 # d = dict(name='bob',age=20,score=88)
 # print json.dumps(d)
 
-json_str = '{"age":20,"score":88,"name":"Bob"}'
-print json.loads(json_str)
+# json_str = '{"age":20,"score":88,"name":"Bob"}'
+# print json.loads(json_str)
+
+
+import json
+def student2dict(std):
+	return {
+		'name' : std.name,
+		'age' :std.age,
+		'score' :std.score
+	}
+
+class Student(object):
+	def __init__(self,name,age,score):
+		self.name = name
+		self.age = age
+		self.score = score
+
+s = Student('Bob',20,88)
+#print s.name
+print(json.dumps(s,default = student2dict))
