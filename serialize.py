f7838237dd67aea0d09cc6a2829ab99dbef7a265
Winlang <rowen0820@gmail.com>
2014-06-15 22:49:46 +0800

    json test

--- serialize.py
+++ serialize.py
@@ -1,7 +1,27 @@
-try:
- 	import cPickle as pickle
-except ImportError:
- 	import pickle
+#pickle
 
-d = dict(name='Bob',age=20,score=88)
-print pickle.dumps(d)
+# try:
+#  	import cPickle as pickle
+# except ImportError:
+#  	import pickle
+
+# d = dict(name='Bob',age=20,score=88)
+# f = open('text.txt','wb')
+# pickle.dump(d,f)
+# f.close()
+#print pickle.dumps(d)
+
+# f = open('text.txt','rb')
+# d = pickle.load(f)
+# f.close()
+# print d 
+
+
+#Json
+
+import json
+# d = dict(name='bob',age=20,score=88)
+# print json.dumps(d)
+
+json_str = '{"age":20,"score":88,"name":"Bob"}'
+print json.loads(json_str)
