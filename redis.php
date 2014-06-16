e5adb2ea895a9e3c77deb6e7b632d5a9bb5ca710
root <root@deepin.deepin-domain>
2014-06-16 23:52:09 +0800

    fix

--- redis.php
+++ redis.php
@@ -1,3 +1,11 @@
 $redis = new redis();
 $result = $redis->connect('127.0.0.1', 6379);
 var_dump($result); //结果：bool(true)
+
+
+$redis = new redis();  
+$redis->connect('127.0.0.1', 6379);  
+$redis->set('test',"1111111111111");  
+echo $redis->get('test');   //结果：1111111111111  
+$redis->delete('test');  
+var_dump($redis->get('test'));  //结果：bool(false)  
